574b8208f7371e7a7f3a14d5fdf94990e0832cfd
Cody C <ccoane@gmail.com>
2018-07-03 20:55:00 -0500

    anotha one

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-//Install express server
+//server.js
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist'));
-app.use(express.static('/dist/koth'));
-
-app.get('/*', function(req,res) {
-    
-//res.sendFile(path.join(__dirname+'/dist/index.html'));
-res.sendFile(path.join('/dist/koth/index.html'));
+const path = require('path');
+app.use(express.static('./dist/koth'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join('./dist/koth/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
