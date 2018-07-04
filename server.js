45c4a991e02a0227ecebdde4857705a9ddcf6be2
Cody C <ccoane@gmail.com>
2018-07-03 20:50:47 -0500

    Using appropriate file path

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // app.use(express.static(__dirname + '/dist'));
-app.use(express.static('/dist/angular-koth-site'));
+app.use(express.static('/dist/koth'));
 
 app.get('/*', function(req,res) {
     
 //res.sendFile(path.join(__dirname+'/dist/index.html'));
-res.sendFile(path.join('/dist/angular-koth-site/index.html'));
+res.sendFile(path.join('/dist/koth/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
